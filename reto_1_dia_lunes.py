435512f3f447214ac696c0cef2e600f7334328a0
Alfredo Ruiz S. <alfredoruizsa@gmail.com>
2024-02-05 19:38:59 -0400

    first commit

---
+++ reto_1_dia_lunes.py
@@ -0,0 +1,10 @@
+"""RETO 1 DE DIA LUNES de Alfredo Ruiz"""
+
+#datos
+Nombre = input("Agrege su Primer Nombre: ")
+Apellido = input("Agrege su Primer Apellido: ")
+Telefono = input("Agrege su Numero Telefonico: ")
+Correo = input("Agrege su Correo Electronico: ")
+
+#mensaje de respuesta 
+print("Hola ",Nombre, Apellido, ",como esta?, sus datos de contactos son: ", Telefono, "y", Correo, "saludos")
